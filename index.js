f46b1479d87fda30c951e23b118bf0e62f449766
imranmammadov12 <Mamed_qf28@itstep.edu.az>
2024-01-06 11:42:31 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+class Task {
+    constructor(title, description, status){
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.id = uniqId();
+        this.createDate = new Date().toLocaleDateString();
+    }
+}
+
+function uniqId() {
+    return Math.random().toString(16).slice(2);
+}
+
+class TasksList {
+    constructor(){
+        this.tasks = [];
+    }
+
+
+    addTask(task) {
+        this.tasks.push(task);
+    }
+
+    getTaskById(id) {
+        return this.tasks.find(task => task.id === id);
+    }
+}
